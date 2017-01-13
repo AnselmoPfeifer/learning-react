34dd33bd75918b19a81e1a96c38208268c3d3547
AnselmoPfeifer <anspfeifer@gmail.com>
2017-01-12 23:26:19 -0300

    Concluindo aula 02

---
+++ 1-curso-javaScript-avançado-ECMAScript-2015/aula02/client/js/app/models/Negociacao.js
@@ -0,0 +1,30 @@
+/**
+ * Created by anselmo on 12/01/17.
+ */
+
+class Negociacao {
+    constructor(data, quatidade, valor){
+        this._data = data;
+        this._quantidade = quatidade;
+        this._valor = valor;
+        Object.freeze(this); // congela a instância do objeto
+
+    }
+
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+
+    get data() {
+        //return this._data;
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+}

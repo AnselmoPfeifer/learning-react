e03b3717ba7a500e8c6bacb6b5876a38561f21cd
AnselmoPfeifer <anspfeifer@gmail.com>
2017-01-15 19:15:13 -0300

    Concluindo aula 02

---
+++ 1-curso-javaScript-avançado-ECMAScript-2015/aula03/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by anselmo on 12/01/17.
+ */
+
+class NegociacaoController {
+
+    adiciona(event) {
+        event.preventDefault();
+        alert('Executou método adiciona');
+    }
+}

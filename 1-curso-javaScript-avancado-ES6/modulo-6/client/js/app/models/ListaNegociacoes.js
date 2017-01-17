2e55096a64ac5320ea173da3c001faf846abc70f
AnselmoPfeifer <anspfeifer@gmail.com>
2017-01-17 17:33:33 -0300

    Finaliando o modulo 06

---
+++ 1-curso-javaScript-avancado-ES6/modulo-6/client/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,21 @@
+/**
+ * Created by anselmo on 16/01/17.
+ */
+
+class ListaNegociacoes {
+
+    constructor() {
+
+        this._negociacoes = [];
+    }
+
+    adiciona(negociacao) {
+
+        this._negociacoes.push(negociacao);
+    }
+
+    get negociacoes() {
+
+        return [].concat(this._negociacoes);
+    }
+}

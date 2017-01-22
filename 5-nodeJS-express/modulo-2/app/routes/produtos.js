77b163ef46bb035c83ee6f769d9ea70ddbfc5bba
AnselmoPfeifer <anspfeifer@gmail.com>
2017-01-22 00:41:37 -0300

    Ajuste em connection separando o codigo

--- 5-nodeJS-express/modulo-2/app/routes/produtos.js
+++ 5-nodeJS-express/modulo-2/app/routes/produtos.js
@@ -6,10 +6,10 @@ module.exports = function (app) {
     app.get('/produtos', function(req,res){
 
         var connection = app.infra.connectionFactory();
+        var produtosbanco = app.infra.produtosBanco
 
-        connection.query('select * from livros',function(err,results){
-            res.render('produtos/lista', {lista:results});
-
+        produtosbanco.lista(connection, function (erros, resultados) {
+            res.render('produtos/lista', {lista:resultados});
         });
 
         connection.end();
